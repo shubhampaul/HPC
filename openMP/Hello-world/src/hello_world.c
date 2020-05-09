6dfe7937e3f0e1efd9aa1c333c02df8ecd184ee2
Shubham Paul <paulplusx@gmail.com>
2020-05-09 16:08:48 +0200

    added num thread clause

--- openMP/Hello-world/src/hello_world.c
+++ openMP/Hello-world/src/hello_world.c
@@ -3,7 +3,7 @@
 
 int main (void) {
     int i = 10;
-    #pragma omp parallel private(i)
+    #pragma omp parallel private(i) num_threads(6)
     {
 	    printf("thread %d: i = %d\n", omp_get_thread_num(), 
 			    omp_get_num_threads());	
