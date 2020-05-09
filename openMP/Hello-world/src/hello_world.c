ab140edb207187788dc243eeeac23e2d5d667d93
Shubham Paul <paulplusx@gmail.com>
2020-05-09 15:20:58 +0200

    init2

---
+++ openMP/Hello-world/src/hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main (void) {
+    int i = 10;
+    #pragma omp parallel private(i)
+    {
+	    printf("thread %d: i = %d\n", omp_get_thread_num(), 
+			    omp_get_num_threads());	
+    }
+    printf("i = %d\n", i);
+
+    return 0;
+}
